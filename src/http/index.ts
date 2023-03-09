25d1483aaec53bce87234ed0c3a21ad8f3ef6f22
talessantos1989 <talesdealmeida@gmail.com>
2023-03-09 15:49:01 -0300

    api url variavel de ambiente

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-const apiURL = process.env.REACT_APP_API_URL;
+// const apiURL = process.env.REACT_APP_API_URL;
 const http = axios.create({
-    baseURL: apiURL,
+    // baseURL: apiURL,
+    baseURL: 'https://backend-formulario-numerologia.vercel.app/',
     withCredentials: false
 })
 
