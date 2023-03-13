768f4d7320ed9b8acd0c94113eee2db3ed484364
talessantos1989 <talesdealmeida@gmail.com>
2023-03-12 21:58:44 -0300

    url alterado para railway após migrar o backend do vercel para a raiway

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // const apiURL = process.env.REACT_APP_API_URL;
 const http = axios.create({
     // baseURL: apiURL,
-    baseURL: 'https://backend-formulario-numerologia.vercel.app/',
+    baseURL: 'https://backend-formulario-numerologia.up.railway.app',
     withCredentials: false
 })
 
