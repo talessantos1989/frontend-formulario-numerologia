d046a77cf3b3ba8bbca3f52649c0d174ceddc3c9
talessantos1989 <talesdealmeida@gmail.com>
2023-03-16 15:42:52 -0300

    correcao baseURL

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 // const apiURL = process.env.REACT_APP_API_URL;
 const http = axios.create({
     // baseURL: apiURL,
-    // baseURL: 'https://backend-formulario-numerologia.up.railway.app',
-    baseURL: 'http://localhost:8001',
+    baseURL: 'https://backend-formulario-numerologia.up.railway.app',
+    //baseURL: 'http://localhost:8001',
     withCredentials: false
 })
 
