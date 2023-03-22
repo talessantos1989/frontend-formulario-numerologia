757d8b0191ec84680c927414f266d720c76d12d1
talessantos1989 <talesdealmeida@gmail.com>
2023-03-22 16:58:43 -0300

    weoir

--- src/components/ReviewForm/index.jsx
+++ src/components/ReviewForm/index.jsx
@@ -3,7 +3,7 @@ import './reviewForm.css'
 
 export default function ReviewForm({data, statusMessage}){
 
-    const datedmy  = Date(data.data_nascimento).toLocaleDateString()
+    const datedmy  = new Date(data.data_nascimento).toLocaleDateString()
     return(
         <>
             <div className="review-container">
